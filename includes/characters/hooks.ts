e60185fc73fac0bb259500b952b4e1da30262a3b
Matt Fantinel <matt@fantinel.dev>
2022-11-18 22:41:28 +0100

    Map API results to the app's type

--- includes/characters/hooks.ts
+++ includes/characters/hooks.ts
@@ -1,22 +1,47 @@
-import useSWR from 'swr';
+import useSWRImmutable from 'swr/immutable'
 
-import { Character } from './types';
+import { ApiResult, CharacterResult } from './types';
 
 const fetcher = async (url: string): Promise<any> => {
   return await fetch(url, {
     method: 'GET'
-  }).then((res) => {
-    // TODO: Map results to Character[]
-    // TODO: Grab results' relational data (planets, films, starships)
-    return res.json();
+  }).then(async (res) => {
+    const apiResult = await res.json() as ApiResult;
+
+    const result: CharacterResult | undefined = apiResult ? {
+      total: apiResult.count,
+      nextPage: apiResult.next ?? undefined,
+      previousPage: apiResult.previous ?? undefined,
+      characters: apiResult.results.map((character) => {
+        return {
+          uid: character.url,
+          name: character.name,
+          height: character.height,
+          weight: character.mass,
+          hairColor: character.hair_color,
+          skinColor: character.skin_color,
+          eyeColor: character.eye_color,
+          birthYear: character.birth_year,
+          gender: character.gender,
+          homeWorld: undefined, // TODO
+          homeWorldUrl: character.homeworld,
+          films: undefined, // TODO
+          filmsUrls: character.films,
+          starships: undefined, // TODO
+          starshipsUrls: character.starships
+        }
+      })
+    } : undefined;
+
+    return result;
   }).catch(err => {
     console.error(err);
   });
 };
 
 export const useCharacters = () => {
-  const { data, error } = useSWR<Character[]>(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/people`, 
+  const { data, error } = useSWRImmutable<CharacterResult>(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/people`,
     (url: string) => fetcher(url)
   );
 
--- includes/characters/types.ts
+++ includes/characters/types.ts
@@ -16,6 +16,13 @@ export type Character = {
   starshipsUrls: string[];
 }
 
+export type CharacterResult = {
+  total: number;
+  nextPage?: string;
+  previousPage?: string;
+  characters: Character[];
+}
+
 export type CharacterApiSchema = {
   birth_year: string;
   eye_color: string;
@@ -29,6 +36,13 @@ export type CharacterApiSchema = {
   skin_color: string;
   starships: string[];
   url: string;
+}
+
+export type ApiResult = {
+  count: number;
+  next: string;
+  previous?: string;
+  results: CharacterApiSchema[];
 }
 
 //
